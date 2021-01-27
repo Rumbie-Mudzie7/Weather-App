e1aeb85d64949c2822b8e1a34a617e7a668bb100
rumbie <rumbiemudzie@gmail.com>
2021-01-27 21:52:40 +0200

    Update backgroundImage url path

--- src/weather.js
+++ src/weather.js
@@ -1,18 +1,18 @@
-function setWeather(choice) {
-  // let mySelect = document.querySelector('select');
+function setWeather() {
+  let mySelect = document.querySelector('#weather-imgs');
   
-  // const choice = mySelect.value;
+  const choice = mySelect.value;
 
   if (choice === "sunny") {
     
-    document.querySelector('body').style.backgroundImage = 'url("/src/images/weather4.jpg")';
-  } else if (choice > "rainy") {
-    document.querySelector('body').style.backgroundImage = 'url("/src/images/weather1.jpg")';
+    document.querySelector('body').style.backgroundImage = 'url("../src/images/weather4.jpg")';
+  } else if (choice === "rainy") {
+    document.querySelector('body').style.backgroundImage = 'url("../src/images/weather1.jpg")';
   } else if (choice === "snowing") {
-    document.querySelector('body').style.backgroundImage = 'url("/src/images/weather4.jpg")';
+    document.querySelector('body').style.backgroundImage = 'url("../src/images/weather4.jpg")';
   } else if (choice === "overcast")  
   {
-    document.querySelector('body').style.backgroundImage = 'url("/src/images/weather2.jpg")';
+    document.querySelector('body').style.backgroundImage = 'url("../src/images/weather2.jpg")';
   } else {
     document.querySelector('body').style.backgroundImage = 'url("")';
   }
