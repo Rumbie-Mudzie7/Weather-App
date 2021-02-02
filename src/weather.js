9c103cd267228bfffdad7814a8381bd629b79c1f
rumbie <rumbiemudzie@gmail.com>
2021-02-02 18:19:16 +0200

    Add null to choice variable as a fall back for empty entry

--- src/weather.js
+++ src/weather.js
@@ -1,22 +1,16 @@
 function setWeather(choice = null) {
- 
-  if(choice === null) {
-    document.querySelector('body').style.backgroundImage = 'url("../src/images/weather5.jpg")';    
+  if (choice === null) {
+    document.querySelector('body').style.backgroundImage = 'url("../src/images/weather5.jpg")';
+  } else if (choice.weather[0].main === 'Clear') {
+    document.querySelector('body').style.backgroundImage = 'url("../src/images/clear-weather.jpeg")';
+  } else if (choice.weather[0].main === 'Clouds') {
+    document.querySelector('body').style.backgroundImage = 'url("../src/images/clouds-weather1.jpeg")';
+  } else if (choice.weather[0].main === 'Rain') {
+    document.querySelector('body').style.backgroundImage = 'url("../src/images/rain-weather4.jpg")';
+  } else if (choice.weather[0].main === 'Mist') {
+    document.querySelector('body').style.backgroundImage = 'url("../src/images/misty-weather3.jpg")';
   } else {
-
-    if (choice.weather[0].main === 'Clear') {
-      
-      document.querySelector('body').style.backgroundImage = 'url("../src/images/clear-weather.jpeg")';
-    } else if (choice.weather[0].main === 'Clouds') {
-      document.querySelector('body').style.backgroundImage = 'url("../src/images/clouds-weather1.jpeg")';
-    } else if (choice.weather[0].main === 'Rain') {
-      document.querySelector('body').style.backgroundImage = 'url("../src/images/rain-weather4.jpg")';
-    } else if (choice.weather[0].main === 'Mist')  
-    {
-      document.querySelector('body').style.backgroundImage = 'url("../src/images/misty-weather3.jpg")';
-    } else {
-      document.querySelector('body').style.backgroundImage = 'url("../src/images/weather5.jpg")';
-    }
+    document.querySelector('body').style.backgroundImage = 'url("../src/images/weather5.jpg")';
   }
 }
 
